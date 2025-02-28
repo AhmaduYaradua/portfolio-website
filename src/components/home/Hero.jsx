d074b256d23b43840884326573f34a689de9e37c
AhmaduYaradua <abuahmada3@gmail.com>
2025-02-28 16:13:29 +0100

    feb commit

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -10,46 +10,6 @@ import TransitionEffect from "../TransitionEffect";
 function Hero() {
   const resume = "/Resume.pdf";
   return (
-    // <section className="flex flex-col md:flex-row container mx-auto gap-44 place-items-center items-center mt-52">
-    //   <div className="ml-11">
-    //     <img
-    //       className="rounded-full w-[600px]"
-    //       src="https://i.pinimg.com/736x/16/b1/2c/16b12ce611d914044ccf74360f99b8ad.jpg"
-    //       alt="."
-    //     />
-    //   </div>
-    //   <div className="text-center">
-    //     <p className="text-xl text-gray-500">Hello, I'm</p>
-    //     <h2 className="text-5xl mx-1">Ahmad Yaradua</h2>
-    //     <h1 className="text-2xl text-gray-300 mb-1">Frontend Developer</h1>
-
-    //     <div>
-    //       <button className="border border-black bg-black text-white 3 p-3 rounded-full mr-2">
-    //         <Link href={resume} download="Resume.pdf">
-    //           Download CV
-    //         </Link>
-    //       </button>
-    //       <button className="border border-black bg-white text-black 3 p-3 rounded-full ml-2">
-    //         <Link href="/contact">Contact info</Link>
-    //       </button>
-    //       <div className="flex gap-2 ml-[9.8rem] mt-2">
-    //         <motion.a
-    //           href="https://github.com/AhmaduYaradua"
-    //           whileHover={{ y: -4 }}
-    //           whileTap={{ scale: 0.9 }}
-    //         >
-    //           <Github className="border rounded-full" />
-    //         </motion.a>
-    //         <motion.a
-    //           href="https://www.linkedin.com/in/ahmad-yaradua-074918142?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B42HyNPmfRiai1lMF7gIU4g%3D%3D"
-    //           whileHover={{ y: -4 }}
-    //         >
-    //           <Linkedin className="border rounded-full" />
-    //         </motion.a>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
     <>
       <TransitionEffect />
       <main className="flex items-center text-black bg-white dark:bg-black w-full min-h-screen dark:text-white 2xl">
@@ -63,25 +23,25 @@ function Hero() {
                 alt="."
               />
             </div>
-            <div className="w-1/2 flex flex-col items-center self-center ml-10 lg:w-full lg:text-center">
+            <div className="w-1/2 flex flex-col sm:ml-10 items-center self-center ml-10 lg:w-full lg:text-center">
               <AnimatedText
                 text="Bringing Ideas to Life with Modern Web Solutions."
                 className="!text-6xl !text-left lg:!text-center xl:!text-5xl lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
-              <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
+              <p className="my-4 sm:hidden text-base font-medium md:text-sm sm:text-xs">
                 I’m a full stack developer with a passion for building creative
                 and efficient web applications. I specialize in React.js and the
                 MERN stack, creating responsive and user-friendly designs. Check
                 out my latest projects to see how I bring ideas to life with
                 modern web technologies.
               </p>
-              <div>
-                <button className="border sm:mb-2 border-black dark:border-white bg-black text-white 3 p-3 rounded-full mr-2">
+              <div className="">
+                <button className="border border-black dark:border-white bg-black text-white sm:w-auto 3 p-3 sm:p-2 rounded-full mr-2">
                   <Link href={resume} download="Resume.pdf" target={"_blank"}>
                     Download CV
                   </Link>
                 </button>
-                <button className="border border-black bg-white text-black 3 p-3 rounded-full ml-2">
+                <button className="border sm:mr-4 sm:mt-2 border-black bg-white text-black 3 p-3 sm:p-2 sm:w-auto rounded-full ml-2">
                   <Link href="mailto:abuahmada3@gmail.com" target={"_blank"}>
                     Contact me
                   </Link>
